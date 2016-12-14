d7feb8f60ca6b04338fe2f30d7d49751d9bc0a58
greicens <gsilva@mail.ccsf.edu>
2016-12-13 18:02:56 -0800

    created route for new book and for update book

--- server.js
+++ server.js
@@ -88,30 +88,33 @@ app.get('/api/books/:id', function(req, res){
   });
 });
 
-// create new book
-// app.post('/api/books', function (req, res) {
-//   // create new book with form data (`req.body`)
-//   console.log('books create', req.body);
-//   var newBook = req.body;
-//   newBook._id = newBookUUID++;
-//   books.push(newBook);
-//   res.json(newBook);
-// });
-//
-// // update book
-// app.put('/api/books/:id', function(req,res){
-// // get book id from url params (`req.params`)
-//   console.log('books update', req.params);
-//   var bookId = req.params.id;
-//   // find the index of the book we want to remove
-//   var updateBookIndex = books.findIndex(function(element, index) {
-//     return (element._id === parseInt(req.params.id)); //params are strings
-//   });
-//   console.log('updating book with index', deleteBookIndex);
-//   var bookToUpdate = books[deleteBookIndex];
-//   books.splice(updateBookIndex, 1, req.params);
-//   res.json(req.params);
-// });
+app.post('/api/books', function(req, res){
+  var newBook = new db.Book(req.body);
+
+  newBook.save(function(err, savedBook){
+    res.json(savedBook);
+  });
+
+});
+
+
+app.put('/api/books/:id', function(req, res){
+  //get bood id from url params
+  var bookId = req.params.id;
+
+  //find book in database by id
+  db.Book.findOne({_id: bookId}, function(err, foundBook){
+    foundBook.title = req.body.title;
+    foundBook.author = req.body.author;
+    // foundBook.image = req.body.image;
+    // foundBook.releaseDate = req.body.releaseDate;
+
+    //save updated book in database
+    foundBook.save(function(err, savedBook){
+      res.json(savedBook);
+    });
+  });
+});
 //
 // // delete book
 // app.delete('/api/books/:id', function (req, res) {
