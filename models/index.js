d692a512840e6f0b3311455a8028be5c21c53b35
greicens <gsilva@mail.ccsf.edu>
2016-12-15 11:42:31 -0800

    added code to fix promise warning

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/book-app");
+mongoose.Promise = global.Promise;
 
 module.exports.Book = require("./book.js");
 module.exports.Author = require("./author.js");
